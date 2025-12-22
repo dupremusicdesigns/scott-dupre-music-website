9ec84a0974787406c83f5c353657cc14f0a5409a
Alex Johnson <johnsonav1992@gmail.com>
2025-12-21 21:40:56 -0600

    forms test

--- src/app/(pages)/contact/page.tsx
+++ src/app/(pages)/contact/page.tsx
@@ -1,7 +1,35 @@
 const ContactPage = () => {
     return (
         <div>
-            page
+            <form
+                name='contact'
+                // @ts-expect-error -> netlify requires this
+                netlify
+            >
+                <p>
+                    <label>
+                        Name
+                        <input
+                            type='text'
+                            name='name'
+                        />
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        Email
+                        <input
+                            type='email'
+                            name='email'
+                        />
+                    </label>
+                </p>
+                <p>
+                    <button type='submit'>
+                        Send
+                    </button>
+                </p>
+            </form>
         </div>
     );
 };
