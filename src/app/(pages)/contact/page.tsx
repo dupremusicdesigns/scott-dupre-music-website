f0c1ca54bb7babeeeefea28b9a4eba7308761f0e
Alex Johnson <johnsonav1992@gmail.com>
2025-12-21 22:00:16 -0600

    probs need this

--- src/app/(pages)/contact/page.tsx
+++ src/app/(pages)/contact/page.tsx
@@ -34,6 +34,7 @@ const ContactPage = () => {
         <div>
             <form
                 name='contact'
+                method='POST'
                 onSubmit={ handleSubmit }
             >
                 <input
