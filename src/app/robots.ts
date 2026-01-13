f3aeb0aaa901f5c5ae7cc2436f54ac7cf5d918a7
Alex Johnson <johnsonav1992@gmail.com>
2026-01-13 04:54:56 -0600

    feat: add dynamic export for static rendering in robots and sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,5 +1,7 @@
 import { MetadataRoute } from 'next';
 
+export const dynamic = 'force-static';
+
 // eslint-disable-next-line import/no-default-export
 export default function robots (): MetadataRoute.Robots {
     const baseUrl = 'https://www.dupremusicdesigns.com';
